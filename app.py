65b4558de2c9c4baa0d12173423264c68fb7d078
nazimbo <113207575+nazimbo@users.noreply.github.com>
2024-02-22 20:49:01 +0100

    Add image processing functionality

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import numpy as np
+from PIL import Image
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        file = request.files["file"]
+        image = Image.open(file.stream)
+        matrix = np.array(image)
+        reshaped = matrix.reshape(-1, 3)
+
+        colors, count = np.unique(reshaped, axis=0, return_counts=True)
+
+        indices = np.argsort(-count)
+
+        top_colors = colors[indices[:10]]
+
+        return render_template('index.html', top_colors=top_colors)
+
     return render_template('index.html')
 
 
