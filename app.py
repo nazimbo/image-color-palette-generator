f3c6067510a66b542c18f8d814d6b20788cd7abe
nazimbo <113207575+nazimbo@users.noreply.github.com>
2024-03-15 12:36:41 +0100

    Remove unused code and update color extraction

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-# import numpy as np
 import colorgram
 from PIL import Image
 
@@ -18,21 +17,7 @@ def home():
         # Get the number of colors to extract
         palette_color_count = request.form.get('color-count')
 
-        # Using numpy to get the colors
-        # matrix = np.array(image)
-        # print(matrix.shape)
-        # reshaped = matrix.reshape(-1, 3)
-        # print(reshaped.shape)
-
-        # colors, count = np.unique(reshaped, axis=0, return_counts=True)
-
-        # indices = np.argsort(-count)
-
-        # top_colors = colors[indices[:int(palette_color_count)]]
-        # top_colors_hex = ['#{:02x}{:02x}{:02x}'.format(
-        #     *rgb) for rgb in top_colors]
-
-        # Using colorgram to get the colors
+        # Extract the colors
         colors = colorgram.extract(file, int(palette_color_count))
         top_colors = [color.rgb for color in colors]
         top_colors_hex = ['#{:02x}{:02x}{:02x}'.format(
