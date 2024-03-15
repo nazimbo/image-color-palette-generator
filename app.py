f39745789406358bac5a527d9c480a7926cf052f
nazimbo <113207575+nazimbo@users.noreply.github.com>
2024-03-15 12:25:30 +0100

    Add colorgram library for extracting colors from images

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
-import numpy as np
+# import numpy as np
+import colorgram
 from PIL import Image
 
 app = Flask(__name__)
@@ -8,20 +9,30 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
+        # Get the file from the request
         file = request.files["file"]
+        # Open the image
         image = Image.open(file.stream)
-        matrix = np.array(image)
-        print(matrix.shape)
-        reshaped = matrix.reshape(-1, 3)
-        print(reshaped.shape)
 
-        colors, count = np.unique(reshaped, axis=0, return_counts=True)
+        # matrix = np.array(image)
+        # print(matrix.shape)
+        # reshaped = matrix.reshape(-1, 3)
+        # print(reshaped.shape)
 
-        indices = np.argsort(-count)
+        # colors, count = np.unique(reshaped, axis=0, return_counts=True)
 
-        palette_color_count = request.form.get('color-count')
+        # indices = np.argsort(-count)
 
-        top_colors = colors[indices[:int(palette_color_count)]]
+        # palette_color_count = request.form.get('color-count')
+
+        # top_colors = colors[indices[:int(palette_color_count)]]
+        # top_colors_hex = ['#{:02x}{:02x}{:02x}'.format(
+        #     *rgb) for rgb in top_colors]
+
+        number_of_colors = request.form.get('color-count')
+
+        colors = colorgram.extract(file, int(number_of_colors))
+        top_colors = [color.rgb for color in colors]
         top_colors_hex = ['#{:02x}{:02x}{:02x}'.format(
             *rgb) for rgb in top_colors]
 
